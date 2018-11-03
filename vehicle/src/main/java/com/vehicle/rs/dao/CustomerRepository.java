736da88791dc80ea832c16b7724f3eb1b980230f
Navya <724453@cognizant.com>
2018-11-03 16:41:50 +0530

    Initial commit

---
+++ vehicle/src/main/java/com/vehicle/rs/dao/CustomerRepository.java
@@ -0,0 +1,22 @@
+package com.vehicle.rs.dao;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CustomerRepository extends JpaRepository<CustomerDTO,Long>{
+	
+	@Query(nativeQuery = true)
+	public List<CustomerDTO> findByCustomer_id(@Param("searchTerm") String searchTerm);
+	@Query(nativeQuery = true)
+	public List<CustomerDTO> getAllCustomers();
+//	@Query(nativeQuery = true)
+//	public List<CustomerDTO> deleteCustomer(@Param("searchTerm") String searchTerm);
+	
+	
+	
+
+}
